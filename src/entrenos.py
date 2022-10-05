ad205139f63536899b5ca1f35adf6fdc0e0f21f6
fermincruz <fcruz@us.es>
2022-10-05 12:18:41 +0200

    Acabdo el ejercicio de lectura

--- src/entrenos.py
+++ src/entrenos.py
@@ -2,52 +2,50 @@ from collections import namedtuple
 import csv
 from datetime import datetime
 
-Entreno = namedtuple('Entreno', 
-'tipo, fechahora, ubicacion, duracion, calorias, distancia, frecuencia, compartido')
+Entreno = namedtuple('Entreno', 'tipo, fechahora, ubicacion, duracion, calorias, distancia, frecuencia, compartido')
 
 def lee_entrenos(ruta_fichero):
     '''
-    Esta función lee un fichero de datos en formato CSV y devuelve una lista
-    de tuplas de tipo Entreno.
+    Esta función lee un fichero CSV con datos de entrenamiento, y devuelve
+    una lista de tuplas de tipo Entreno.
 
-    Un ejemplo de las líneas del fichero CSV esperado es:
-    Andar,12/11/2021 8:14,Sevilla,48,155,3.49,89,N
+    Parámetros:
+     - ruta_fichero: la ruta del fichero CSV, de tipo str.
 
-    Parámetro de entrada:
-        ruta_fichero: la ruta del fichero CSV, de tipo str.
-    
-    Valor devuelto:
-        Lista de tuplas de tipo Entreno.
+    Valor de devolución:
+     - lista de tuplas de tipo Entreno.
     '''
     with open(ruta_fichero, encoding='utf-8') as f:
         lector = csv.reader(f)
         next(lector)
         lista_entrenos = []
-        for tipo, fechahora, ubicacion, duracion,\
-             calorias, distancia, frecuencia, compartido in lector:
-             fechahora = parsea_fechahora(fechahora)
-             duracion = int(duracion)
-             calorias = int(calorias)
-             distancia = float(distancia)
-             frecuencia = int(frecuencia)
-             compartido = parsea_logico(compartido)
-             lista_entrenos.append(
-                Entreno(tipo, fechahora, ubicacion, duracion,
-                         calorias, distancia, frecuencia, compartido)
-             )
+        for tipo, fechahora, ubicacion\
+            ,duracion, calorias, distancia\
+            ,frecuencia, compartido in lector:
+                fechahora = parsea_fechahora(fechahora)
+                duracion = int(duracion)
+                calorias = int(calorias)
+                distancia = float(distancia)
+                frecuencia = int(frecuencia)
+                compartido = parsea_logico(compartido)
+                lista_entrenos.append(
+                    Entreno(tipo, fechahora, ubicacion, duracion, calorias, distancia, frecuencia, compartido)
+                )
         return lista_entrenos
 
 def parsea_fechahora(cadena):
     '''
-    Recibe una cadena con el formato "dia/mes/año hora:minutos",
-    y devuelve un objeto datetime con esa información
+    Recibe una cadena de texto con una fecha y hora con el formato:
+        dia/mes/año hora:minutos
+
+    , y devuelve un objeto de tipo datetime con esa información.
     '''
     return datetime.strptime(cadena, "%d/%m/%Y %H:%M")
 
 def parsea_logico(cadena):
     '''
-    Recibe una cadena con 'S' o 'N', y devuelve True
-    en el primero caso, y False en el segundo caso.
+    Recibe una cadena de texto que puede ser 'S' o 'N', devolviendo
+    True en el primer caso y False en el segundo.
     '''
     if cadena == 'S':
         return True
--- src/entrenos_test.py
+++ src/entrenos_test.py
@@ -1,13 +1,11 @@
 from entrenos import *
 
-datos = lee_entrenos("data/entrenos.csv")
-print(f"Se han leído {len(datos)} datos.")
+registros = lee_entrenos("data/entrenos.csv")
+
+print(f"Leídos {len(registros)} registros.")
 
 print("\nMostrando los tres primeros registros:")
-print(datos[:3])
+print(registros[:3])
 
 print("\nMostrando los tres últimos registros:")
-print(datos[-3:])
-
-
-
+print(registros[-3:])
